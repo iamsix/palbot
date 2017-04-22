3f731f62836be3da03a2b934b00944a7b530539b
iamsix <github@sixis.ca>
2017-04-22 10:36:45 -0600

    make an mtg command leave the line parser there

--- botmodules/mtg.py
+++ botmodules/mtg.py
@@ -12,6 +12,25 @@ def card_scraper (self, e):
     cards = re.findall(CARD_REGEX, e.input)
 
     for card, cset in cards:
+        e.output += get_card(card, cset)
+    e.allowembed = True
+    return e
+card_scraper.lineparser = True
+
+def mtg_cmd (self, e):
+    card = e.input.strip()
+    cset = ""
+    cres = re.search("\s\(([A-z]+)\)", e.input)
+    if cres:
+        cset = cres.group(1).strip()
+        card = card.replace(cres.group(0), "").strip()
+
+    e.output = get_card(card, cset)
+    e.allowembed = True
+    return e
+mtg_cmd.command = "!mtg"        
+
+def get_card (card, cset=""):
         card = urllib.parse.quote(card.strip())
         cset = urllib.parse.quote(cset.strip())
 
@@ -23,15 +42,14 @@ def card_scraper (self, e):
             opener = urllib.request.build_opener()
             opener.addheaders = [('User-agent', 'Palbot for discord/2.0')]
             response = opener.open(url).read()
-        except urllib.error.HTTPError as e:
-            print(e)
-            print(e.read())
+        except urllib.error.HTTPError as err:
+            print(err)
+            print(err.read())
         data = response.decode()
 
         cards = json.loads(data)['cards']
         if not cards:
-            e.output += "No card found for [{}]\n".format(card)
-            continue
+            return "No card found for [{}]\n".format(card)
 
         data = None
         image = "No image found"
@@ -44,14 +62,10 @@ def card_scraper (self, e):
                 pass
 
 
-        e.output += "**{}** ({}) \n {} \n".format(data["name"],
-                                                  data["set"],
-                                                  image)
+        return "**{}** ({}) \n {} \n".format(data["name"],
+                                             data["set"],
+                                             image)
 
-    e.allowembed = True
-    print(e.output)
-    return e
-card_scraper.lineparser = True
 
 class test:
     pass
@@ -60,4 +74,9 @@ test.source.name = "mtg_nerds"
 test.input = "check out the [hinder (chk)] to [tunnel vision] combo"
 test.output = ""
 card_scraper(None, test)
-
+print("card scraper test:")
+print(test.output)
+print("----")
+test.input = "hinder (chk)"
+mtg_cmd(None, test)
+print(test.output)
