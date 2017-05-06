da3189f4df511e641a0e82dc57e2abcc57f282b5
iamsix <github@sixis.ca>
2017-05-05 23:45:52 -0600

    add drinks module minor trivia changes

---
+++ botmodules/drinks.py
@@ -0,0 +1,49 @@
+import urllib.request
+import urllib.parse
+import json
+
+def drinks(self, e):
+    pass
+    url = "http://www.thecocktaildb.com/api/json/v1/1/search.php?s={}"
+    url = url.format(urllib.parse.quote(e.input))
+
+    response = urllib.request.urlopen(url).read()
+    response = json.loads(response.decode())
+
+   # print(response)
+    drink = response["drinks"][0]
+
+    print(drink)
+
+    #this API is a bit odd that it doesn't make these a list  to iterate them
+    #it just lists 1 through 15 each time...
+    ingredients = ""
+    for ingr in range(1,15):
+        if drink["strIngredient{}".format(ingr)]:
+            ingredient = drink["strIngredient{}".format(ingr)]
+            ingredient = ingredient.replace("\n", "").strip()
+            measure = drink["strMeasure{}".format(ingr)]
+            measure = measure.replace("\n", "").strip()
+            ingredients += "{} {}\n".format(measure, ingredient)
+    ingredients = ingredients[:-1]
+
+    output = """**{}** - {}
+{}
+Instructions: {}
+Glass: {}"""
+
+    e.output = output.format(drink["strDrink"], drink["strCategory"], 
+                             ingredients,
+                             drink["strInstructions"],
+                             drink["strGlass"])
+
+drinks.command = "!drink"
+
+
+class drinktest:
+     pass
+
+drinktest.input = "vodka martini"
+drinktest.output = ""
+drinks(None, drinktest)
+print(drinktest.output)
