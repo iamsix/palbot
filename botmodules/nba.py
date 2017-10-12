68db01fdff2a07199fb6dea48fa23018887b7051
iamsix <github@sixis.ca>
2017-10-12 00:50:19 -0600

    new nba API an actual api instead of the shens before also add 'tomorrow' and 'yesterday' to nhl

--- botmodules/nba.py
+++ botmodules/nba.py
@@ -1,19 +1,98 @@
-import re
+import json
 import urllib.request
+import datetime
+from datetime import timedelta
 
-def nba(self, e):
-    url = "http://scores.espn.go.com/nba/bottomline/scores"
-    request = urllib.request.urlopen(url)
-    data = request.read().decode()
 
-    data = data.replace("%20%20%20", " - ")
-    data = data.replace('%20',' ').replace('^','').replace('&','\n')
-    game = ""
-    pattern = re.compile("nba_s_left\d+=(.*)")
-    for match in re.findall(pattern, data):
-        game = game + match + " | "
-    game = game[:-2]
+TEAMS = {
+	"ATL" : "Hawks",
+	"BOS" : "Celtics",
+	"BKN" : "Nets",
+	"CHA" : "Hornets",
+	"CHI" : "Bulls",
+	"CLE" : "Cavaliers",
+	"DAL" : "Mavericks",
+	"DEN" : "Nuggets",
+	"DET" : "Pistons",
+	"GSW" : "Warriors",
+	"HOU" : "Rockets",
+	"IND" : "Pacers",
+	"LAC" : "Clippers",
+	"LAL" : "Lakers",
+	"MEM" : "Grizzlies",
+	"MIA" : "Heat",
+	"MIL" : "Bucks",
+	"MIN" : "Timberwolves",
+	"NOP" : "Pelicans",
+	"NYK" : "Knicks",
+	"OKC" : "Thunder",
+	"ORL" : "Magic",
+	"PHI" : "Sixers",
+	"PHX" : "Suns",
+	"POR" : "Trail Blazers",
+	"SAC" : "Kings",
+	"SAS" : "Spurs",
+	"TOR" : "Raptors",
+	"UTA" : "Jazz",
+	"WAS" : "Wizards"
+}
 
-    e.output = game
-nba.command = "!nba"
+def team(abbr):
+    try:
+        return TEAMS[abbr]
+    except:
+        return abbr
+
+def get_nba_games(self, e):
+    if e.input:
+        if e.input.lower() == "tomorrow":
+            gameday = datetime.date.today() + timedelta(days=1)
+        elif e.input.lower() == "yesterday":
+            gameday = datetime.date.today() - timedelta(days=1)
+        else:
+            gameday = datetime.datetime.strptime(e.input, "%Y-%m-%d")
+    else:
+        gameday = datetime.date.today()
+         
+    url = "https://data.nba.net/prod/v2/{}/scoreboard.json"
+    url = url.format(gameday.strftime("%Y%m%d"))
+    data = urllib.request.urlopen(url).read().decode()
+    data = json.loads(data)
+    
+    games = []
+    for game in data['games']:
+        
+        if game['statusNum'] == 1:
+            # Game is scheduled in the future
+            gametxt = "{} @ {} ({})"
+            gametxt = gametxt.format(team(game['vTeam']['triCode']),
+            	                     team(game['hTeam']['triCode']),
+            	                     game['startTimeEastern'].replace(':00', ''))
+            	                     
+            	                     
+        else:
+            # game is finished or currently on
+            gametxt = "{} {} - {} {} ({})"
+            
+            if game['statusNum'] == 2: 
+                status = "{} Q{}".format(game['clock'], game['period']['current'])
+            else:
+                status = "Final"
+                
+            gametxt = gametxt.format(team(game['vTeam']['triCode']),
+            	                     team(game['vTeam']['score']),
+            	                     team(game['hTeam']['score']),
+            	                     team(game['hTeam']['triCode']),
+            	                     status)
+        games.append(gametxt)
+         
+    e.output = " | ".join(games)
+    return e
+     
+
+get_nba_games.command = "!nba"
+
+#get_nba_games.input = "tomorrow"
+#get_nba_games(None, get_nba_games)
+#print(get_nba_games.output)
 
--- botmodules/nhl.py
+++ botmodules/nhl.py
@@ -1,17 +1,26 @@
 import json
 import urllib.request
 import datetime
+from datetime import timedelta
 import pytz
+
 
 ET = pytz.timezone("US/Eastern")
 
 def get_nhl_games(self, e):
     if e.input:
-        today = e.input
+        if e.input.lower() == "tomorrow":
+            gameday = datetime.date.today() + timedelta(days=1)
+        elif e.input.lower() == "yesterday":
+            gameday = datetime.date.today() - timedelta(days=1)
+        else:
+            gameday = datetime.datetime.strptime(e.input, "%Y-%m-%d")
     else:
-        today = datetime.date.today().strftime("%Y-%m-%d")
+        gameday = datetime.date.today()
+
         
-    url = "http://statsapi.web.nhl.com/api/v1/schedule?startDate={0}&endDate={0}&expand=schedule.teams,schedule.linescore".format(today)
+    url = "http://statsapi.web.nhl.com/api/v1/schedule?startDate={0}&endDate={0}&expand=schedule.teams,schedule.linescore"
+    url = url.format(gameday.strftime("%Y-%m-%d"))
     data = urllib.request.urlopen(url).read().decode()
     data = json.loads(data)
     data = data['dates'][0]
@@ -51,6 +60,7 @@ def get_nhl_games(self, e):
         
 get_nhl_games.command = "!nhl"
 
-get_nhl_games.input = "2017-10-07"
-get_nhl_games(None,  get_nhl_games)
-print(get_nhl_games.output)
+#get_nhl_games.input = "yesterday"
+#get_nhl_games(None,  get_nhl_games)
+#print(get_nhl_games.output)
+
