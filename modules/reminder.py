8d57f7962d2d9e4a1ed3a3b17b81f9a465bd4625
iamsix <github@sixis.ca>
2025-03-06 23:26:46 +0000

    Reminder interactions

--- modules/reminder.py
+++ modules/reminder.py
@@ -18,6 +18,75 @@ class ReminderItem:
     when: datetime
     message: str
 
+UTC = ZoneInfo("UTC")
+
+class TimePrompt(discord.ui.Modal, title="New reminder time"):
+    new_time = None
+    reminder: ReminderItem = None
+    time = discord.ui.TextInput(label="New Time for the reminder", required=True)
+    async def on_submit(self, interaction: discord.Interaction):
+        userinfo = interaction.client.utils.AuthorInfo(interaction.user)
+        tz = userinfo.timezone
+        date = dateparser.parse(self.time.value , 
+                                settings={'TIMEZONE': tz, 
+                                'PREFER_DATES_FROM': 'future',
+                                'RETURN_AS_TIMEZONE_AWARE': True},
+                                )
+        if date:
+            self.new_time = date
+            await interaction.response.defer()
+        else:
+            await interaction.response.send_message(
+                f"Failed to parse new date / time. Push button again to try again", 
+                ephemeral=True
+                )
+
+
+class EditReminderView(discord.ui.View):
+    reminder_item: ReminderItem = None
+    reminder = None
+    message = None
+    task = None
+    @discord.ui.button(label="Change time", emoji="⏱️", style=discord.ButtonStyle.gray)
+    async def on_click_change(self, interaction: discord.Interaction, button):
+        if interaction.user.id == self.reminder_item.user:
+            time = TimePrompt()
+            time.reminder = self.reminder_item
+            await interaction.response.send_modal(time)
+            await time.wait()
+            if time.new_time:
+                await self.reminder.delete_timer(self.reminder_item)
+                self.reminder_item.when = time.new_time
+                if self.task:
+                    self.reminder.reminders.remove(self.task)
+                    self.task.cancel()
+                out, task = await self.reminder.make_reminder(self.reminder_item)
+                self.message.content = out
+                if task:
+                    self.task = task
+                await interaction.followup.edit_message(self.message.id, content=out, view=self)
+        else:
+            await interaction.response.send_message("You didn't create this reminder", ephemeral=True)
+
+    @discord.ui.button(label="Delete Reminder", emoji="🗑️", style=discord.ButtonStyle.red)
+    async def on_click_delete(self, interaction: discord.Interaction, button):
+        if interaction.user.id == self.reminder_item.user:
+            await self.reminder.delete_timer(self.reminder_item)
+            if self.task:
+                self.reminder.reminders.remove(self.task)
+                self.task.cancel()
+            await interaction.response.edit_message(content="Reminder deleted.", view=None)
+            self.stop()
+        else:
+            await interaction.response.send_message("You didn't create this reminder", ephemeral=True)
+    
+    async def on_timeout(self):
+        try:
+            self.clear_items()
+            await self.message.edit(content=self.message.content, view=None)
+        except Exception as e:
+            print(e)
+
 
 class Reminder(commands.Cog):
     reminders = set()
@@ -34,17 +103,23 @@ class Reminder(commands.Cog):
 
 
     async def start_timer(self, reminder):
-        seconds = max(0,int((reminder.when - datetime.utcnow()).total_seconds()))
+        seconds = max(0,int((reminder.when - datetime.now(UTC)).total_seconds()))
         await asyncio.sleep(seconds)
         await self.call_reminder(reminder)
 
 
-    async def save_timer(self, reminder):
+    async def save_timer(self, reminder: ReminderItem):
         q = "INSERT INTO reminders VALUES (?, ?, ?, ?, ?)"
         self.c.execute(q, (int(reminder.when.timestamp()), 
                            reminder.channel, reminder.user, 
                            str(reminder.when), reminder.message))
         self.conn.commit()
+
+    async def delete_timer(self, reminder: ReminderItem):
+        q = "DELETE FROM reminders WHERE timestamp = (?)"
+        self.c.execute(q, [int(reminder.when.timestamp())])
+        self.conn.commit()
+
 
     @tasks.loop(minutes=(60 * 24))
     async def check_timers(self):
@@ -53,7 +128,7 @@ class Reminder(commands.Cog):
         for reminder in self.reminders:
             reminder.cancel()
         self.reminders.clear()
-        ts = int(datetime.utcnow().timestamp())
+        ts = int(datetime.now(UTC).timestamp())
         ts += 24*60*60
         q = 'SELECT timestamp, channel, user, reminder FROM reminders WHERE timestamp <= ?'
         res = self.c.execute(q, [(ts)])
@@ -69,6 +144,7 @@ class Reminder(commands.Cog):
         task = asyncio.create_task(self.start_timer(reminder))
         self.reminders.add(task)
         task.add_done_callback(self.reminders.discard)
+        return task
 
 
     def reminder_parser(self, line, tz):
@@ -76,8 +152,12 @@ class Reminder(commands.Cog):
         date = None
         message = ""
         for i in range(len(words)):
-            tempdate = dateparser.parse(" ".join(words[:i+1]), settings={'TIMEZONE': tz, 
-                                      'PREFER_DATES_FROM': 'future'})
+            tempdate = dateparser.parse(
+                " ".join(words[:i+1]), 
+                settings={'TIMEZONE': tz, 
+                        'PREFER_DATES_FROM': 'future',
+                        'RETURN_AS_TIMEZONE_AWARE': True},
+                )
             if tempdate:
                 date = tempdate
                 skip = 1
@@ -102,6 +182,7 @@ class Reminder(commands.Cog):
         if date:
             await ctx.send(f"<t:{int(date.timestamp())}> {message}")
 
+    # might make this a command group? to delete reminders.
     @app_commands.command()
     @app_commands.guild_only()
     async def remind(self, interaction: discord.Interaction, when: str, what: str):
@@ -116,36 +197,40 @@ class Reminder(commands.Cog):
         """
         userinfo = interaction.client.utils.AuthorInfo(interaction.user)
         tz = userinfo.timezone
-        date = dateparser.parse(when, settings={'TIMEZONE': tz, 'PREFER_DATES_FROM': 'future'})
-        usertime = datetime.strftime(date, "%a, %B %-d, %Y at %-I:%M:%S %p %Z")
-        if tz:
-            ntz = ZoneInfo(tz)
-            utc = ZoneInfo("UTC")
-            date = date.replace(tzinfo=ntz).astimezone(tz=utc).replace(tzinfo=None)
-        seconds = int((date - datetime.utcnow()).total_seconds())
-        reminder = ReminderItem(interaction.channel.id, interaction.user.id, date, what)
-        if seconds < 0:
+        date = dateparser.parse(when, settings={'TIMEZONE': tz, 
+                                                'PREFER_DATES_FROM': 'future', 
+                                                'RETURN_AS_TIMEZONE_AWARE': True})
+        if not date:
             await interaction.response.send_message(
-                "I can't remind you of something in the past", 
+                "I don't understand when you want this done. Try something like `tomorrow at 8pm` or `jan 3rd 2pm` or `in 5 minutes`",
                 ephemeral=True,
                 )
             return
-        elif seconds > (24 * 60 * 60):
-            #save this for later
-            pass
-        else:
-            #this is today so set the timer immediately
-            await self.set_timer(reminder)
-        await self.save_timer(reminder)
-        await interaction.response.send_message(
-            f"OK I'll remind you on {usertime}", 
-            ephemeral=True,
-            )
+        
+        reminder = ReminderItem(interaction.channel.id, interaction.user.id, date, what)
+        out, task = await self.make_reminder(reminder)
+        editbutton = EditReminderView(timeout=60)
+        editbutton.reminder = self
+        editbutton.reminder_item = reminder
+        editbutton.task = task
+        
+        msg = await interaction.response.send_message(out, view=editbutton, ephemeral=True)
+        editbutton.message = msg.resource
 
+    # @remind.command(name="delete")
+    # @app_commands.describe(id='The reminder ID')
+    # @app_commands.autocomplete(id=self.reminders_choices)
+    # I think I want to makke id a Choice thing instead of autocomplete
+    # @app_commands.choices(id=reminders_choices) # not sure if I can provide the interaction?
+    async def delete_reminder(self, interaction: discord.Interaction, id: int):
+        pass
+
+    async def reminders_choices(interaction: discord.Interaction, id: int):
+        pass
 
     @commands.command(aliases=['remind'])
     @commands.guild_only()
-    async def remindme(self, ctx, *, message: commands.clean_content):
+    async def remindme(self, ctx, *, message: str):
         if ctx.invoked_with.lower() == "remind" and message[:3] == "me ":
             message = message[3:]
             # print(message)
@@ -155,31 +240,39 @@ class Reminder(commands.Cog):
             await ctx.reply("I don't understand when you want this done. Try something like `tomorrow at 8pm` or `jan 3rd 2pm` or `in 5 minutes` - format is `!remindme <when> <what>`")
             return
 
-        if tz:
-            ntz = ZoneInfo(tz)
-            utc = ZoneInfo("UTC")
-            date = date.replace(tzinfo=ntz).astimezone(tz=utc).replace(tzinfo=None)
-        seconds = int((date - datetime.utcnow()).total_seconds())
         reminder = ReminderItem(ctx.channel.id, ctx.author.id, date, what)
+        out, task = await self.make_reminder(reminder)
+        
+        editbutton = EditReminderView(timeout=60)
+        editbutton.reminder = self
+        editbutton.reminder_item = reminder
+        editbutton.task = task
+
+        msg = await ctx.send (out, view=editbutton)
+        editbutton.message = msg
+    
+
+    async def make_reminder(self, reminder: ReminderItem):
+        if reminder.when.tzinfo:
+            reminder.when = reminder.when.astimezone(tz=UTC)
+        seconds = int((reminder.when - datetime.now(UTC)).total_seconds())
+        task = None
         if seconds < 0:
-            await ctx.send("I can't remind you of something in the past")
-            return
-        elif seconds > (24 * 60 * 60):
-            #save this for later
-            pass
+            out = "I can't remind you of something in the past"
         else:
-            #this is today so set the timer immediately
-            await self.set_timer(reminder)
+            out = f"I will remind you on <t:{int(reminder.when.timestamp())}>: {reminder.message}"
+            if seconds < (24 * 60 * 60):
+                task = await self.set_timer(reminder)
         await self.save_timer(reminder)
 
-
-        await ctx.send (f"I will remind you on <t:{int(date.timestamp())}>: {what}")
+        return (out, task)
 
     async def call_reminder(self, reminder):
         channel = self.bot.get_channel(reminder.channel)
         msg = f'<@{reminder.user}>: {reminder.message}'
-
-        await channel.send(msg)
+        user = self.bot.get_user(reminder.user)
+        allowed_mentions = discord.AllowedMentions(users=[user])
+        await channel.send(msg, allowed_mentions=allowed_mentions)
 
         q = 'DELETE FROM reminders WHERE timestamp <= ?'
         self.c.execute(q, [(int(reminder.when.timestamp()))])
